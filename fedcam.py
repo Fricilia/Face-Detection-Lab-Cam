19fc22f55682c4d6409d248dd54bb7a9bcf4e190
Fricilia <73948486+Fricilia@users.noreply.github.com>
2022-06-30 18:18:33 +0700

    Add files via upload

---
+++ fedcam.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+
+videoCam = cv2.VideoCapture(0)
+
+face = cv2.CascadeClassifier('face-detect.xml')
+eye = cv2.CascadeClassifier('eye-detect.xml')
+
+while True:
+    cond, frame = videoCam.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    muka = face.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in muka:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 5)
+
+        roi_warna = frame[y:y+h, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w]
+        mata = eye.detectMultiScale(roi_gray)
+        for (mx,my,mw,mh)in mata:
+            cv2.rectangle(roi_warna, (mx,my), (mx+mw, my+mh), (255,255,0), 2)
+
+    cv2.imshow('Face dan Eye detection', frame)
+
+    k = cv2.waitKey(1) & 0xff
+    if k == ord('q'):
+        break
+
+videoCam.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
